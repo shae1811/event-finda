4b42ada8a66d6ec8221f4d1af88eb97d6032c8f2
Shae <shae1811@gmail.com>
2019-09-07 11:37:15 +0800

    Looking good so far...

--- eventFinderApp/models.py
+++ eventFinderApp/models.py
@@ -6,7 +6,11 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     start_time = models.DateTimeField('start time and date')
     end_time = models.DateTimeField('end time and date')
-
+    venue = models.CharField(max_length=200, null=True)
+    category = models.ManyToManyField('Category')
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    
+    # def __str__(self):
+    #     return self.name
--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'freeeventfinder', # you will need to create this db
-        'USER': '', # enter your user name here
-        'PASSWORD': '',
+        'USER': 'shaepokrywka', # enter your user name here
+        'PASSWORD': 'Crawford144!',
         'HOST': 'localhost',
         'PORT': '5432',
     }
